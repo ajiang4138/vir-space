4bc9a8d9f42d32c358030a3fcc530e4a14192d40
Allen Jiang <cupnoodles4138@gmail.com>
2026-04-04 21:33:17 -0400

    fix

--- client/src/components/WhiteboardPanel.tsx
+++ client/src/components/WhiteboardPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ReactSketchCanvas, ReactSketchCanvasRef } from "react-sketch-canvas";
 
 interface WhiteboardPanelProps {
